58c21b982d2f9d64a77b47cc13e2078642c78d95
jessezimm <jessezimm@gmail.com>
2021-11-17 21:45:08 -0500

    module 9

---
+++ app.py
@@ -0,0 +1,40 @@
+
+from flask import Flask
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import jsonify
+#from flask_mobility import Mobility
+
+#app = Flask(__name__)
+#@app.route('/')
+#@app.route('/')
+#def hello_world():
+    #return 'Hello world'
+app = Flask(__name__)
+@app.route("/")
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    precipitation = session.query(Measurement.date, Measurement.prcp).\
+      filter(Measurement.date >= prev_year).all()
+    precip = {date: prcp for date, prcp in precipitation}
+    return jsonify(precip)
+
+if __name__ == "__main__":
+    app.run(debug=True)
